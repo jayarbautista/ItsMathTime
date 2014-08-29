006b812e3a272fcee9aa7c50d28fa24f587a43ad
jayarbautista <j.bautista@klab.com>
2014-08-29 09:24:01 +0800

    Main Menu Screen layout has been updated

--- ItsMathTime/src/com/jayar/project/itsmathtime/ItsMathTimeActivity.java
+++ ItsMathTime/src/com/jayar/project/itsmathtime/ItsMathTimeActivity.java
@@ -1,34 +1,17 @@
 package com.jayar.project.itsmathtime;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ItsMathTimeActivity extends ActionBarActivity {
+public class ItsMathTimeActivity extends SingleFragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_its_math_time);
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return new ItsMathTimeFragment();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.its_math_time, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 }
